c0226d06bbb730e2c61621e231a34f1d85233491
Andrew Nattress <a.nattress@gmail.com>
2021-01-29 23:47:49 +0000

    ship controller

---
+++ Assets/PlayerSetShipTargetLocation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(ShipController))]
+public class PlayerSetShipTargetLocation : MonoBehaviour
+{
+    public ShipController controller;
+    public Transform cursorTransform;
+    public Camera mainCamera;
+    public LayerMask groundLayers;
+    private void Start()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        if (Mouse.current.leftButton.IsPressed())
+        {
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out hit, 50000.0f, groundLayers))
+            {
+                cursorTransform.position = new Vector3(hit.point.x, 0, hit.point.z);
+                
+            }
+        }
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+            controller.target = cursorTransform;
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+            controller.target = null;
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipController : MonoBehaviour
+{
+    public IntVariable playerShips;
+    public Transform target = null;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+    public float accelerationSpeed;
+    public float maxSpeed;
+    private float speed = 0.0f;
+
+    void Start()
+    {
+
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            MoveTowards();
+        }
+        else
+        {
+            speed = rb.velocity.magnitude;
+        }
+    }
+
+    private void MoveTowards()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 2.0f)
+        {
+            speed = Mathf.Clamp(Vector3.Distance(transform.position, target.position) - 1.0f, 0.0f, 100.0f);
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, maxSpeed, Time.deltaTime * accelerationSpeed);
+        }
+
+
+        var lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed * Mathf.Clamp(speed, 0.0f, 1.0f));
+
+        rb.velocity = transform.forward * speed;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (target != null)
+            Gizmos.DrawWireSphere(target.position, 2.0f);
+    }
+}
