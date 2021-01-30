bc8fcd22e6638b79519ee11463a5dc22e9f46cbc
Andrew Nattress <a.nattress@gmail.com>
2021-01-30 14:28:18 +0000

    ship controller now accelerates/slows down

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-//[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class ShipController : MonoBehaviour
 {
     public float accelerationSpeed;
@@ -11,19 +11,16 @@ public class ShipController : MonoBehaviour
     public Transform target;
     public NavMeshAgent agent;
 
-    Vector3 lastDir;
     private void Update()
     {
         if (target)
         {
             agent.speed = Mathf.Lerp(agent.speed, maxSpeed, Time.deltaTime * accelerationSpeed);
             agent.destination = target.position;
-            lastDir = transform.position - target.position;
         }
         else
         {
             agent.speed = Mathf.Lerp(agent.speed, 0, Time.deltaTime * accelerationSpeed);
-            agent.destination = transform.position;
         }
     }
     private void OnDrawGizmos()
