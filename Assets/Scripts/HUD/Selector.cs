17d318c1b196f4ccc9ac8b1b1a7e8668a6225162
rtm516 <ryantmilner@hotmail.co.uk>
2021-01-30 17:41:46 +0000

    Add selector to main menu

---
+++ Assets/Scripts/HUD/Selector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class Selector : MonoBehaviour
+{
+	private TMP_Text text;
+	private RectTransform rectTransform;
+
+	private void Start()
+	{
+		text = GetComponent<TMP_Text>();
+		rectTransform = GetComponent<RectTransform>();
+		text.color = new Color(text.color.r, text.color.g, text.color.b, 0f);
+	}
+
+	public void OnEnter(BaseEventData data)
+	{
+		PointerEventData pointerEvent = (PointerEventData)data;
+
+		RectTransform rect = pointerEvent.pointerCurrentRaycast.gameObject.transform.parent.GetComponent<RectTransform>();
+		rectTransform.localPosition = rect.offsetMin;
+
+		text.color = new Color(text.color.r, text.color.g, text.color.b, 1f);
+	}
+
+	public void OnLeave(BaseEventData data)
+	{
+		text.color = new Color(text.color.r, text.color.g, text.color.b, 0f);
+	}
+}
