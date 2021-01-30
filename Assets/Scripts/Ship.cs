7184e34386189b7ea213bedc95c96d45344b2203
Kitkatann <44240290+Kitkatann@users.noreply.github.com>
2021-01-30 15:32:24 +0000

    fleet class, more ship combat setup

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     public float health;
-    public enum fleetColour{ red, blue, green };
+    
     public float strength;
+    public Vector3 position;
+    public bool attacking;
 
-    public fleetColour fleet;
+    private int cooldown;
+    
 
-    public Ship(fleetColour myFleet)
+    public Ship()
     {
         health = 100;
         strength = 2;
-        fleet = myFleet;
+        position = new Vector3(0, 0, 0);
+        cooldown = 100;
+        attacking = false;
     }
 
     public void takeDamage(float damage)
     {
         health -= damage;
+    }
+
+    public void attack(Ship target)
+    {
+        if (cooldown <= 0)
+        {
+            // Do damage to other ship
+            target.takeDamage(strength);
+            attacking = true;
+            cooldown = 100;
+        }
     }
 
     // Start is called before the first frame update
@@ -31,6 +47,9 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (attacking == true)
+        {
+            cooldown -= 1;
+        }
     }
 }
--- Assets/Scripts/ShipCombat.cs
+++ Assets/Scripts/ShipCombat.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class ShipCombat : MonoBehaviour
 {
-    private ArrayList playerFleet;
-    private ArrayList enemyFleets;
+    private List<Fleet> enemyFleets;
+    private Fleet playerFleet;
 
     public ShipCombat()
     {
+        enemyFleets = new List<Fleet>();
+    }
 
+    public void attack(Ship attacker, Ship attacking)
+    {
+        // Do some particles for a cannon blast
+        // TO DO
+        // Do damage to other ship
+        attacker.attack(attacking);
+    }
+
+    public bool withinRange(Vector3 pos1, Vector3 pos2)
+    {
+        float dist = Vector3.Distance(pos1, pos2);
+        if (dist < 10)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool checkForEnemies()
+    {
+        for (int i = 0; i < enemyFleets.Count; i++)
+        {
+            if (withinRange(playerFleet.center, enemyFleets[i].center))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     // Start is called before the first frame update
