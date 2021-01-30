0012a2c595ca4f3dced7a489955b18430ae84ebd
Patton97 <atpatton97@gmail.com>
2021-01-30 20:11:36 +0000

    Camera Zoom

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public float damping;
     Vector3 offset;
+    float zoomScale = 100f;
+    const float MIN_ZOOM = 10f;
+    const float MAX_ZOOM = 100f;
+
     void Start()
     {
         offset = transform.position - target.position;
@@ -15,8 +19,28 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 point = Vector3.Lerp(transform.position, target.position + offset, Time.time * damping);
+        Vector3 point = Vector3.Lerp(transform.position, target.position + (offset.normalized *  zoomScale), Time.time * damping);
         transform.position = point;
         transform.LookAt(point);
+    }
+
+    void ChangeZoom(float amount)
+    {
+        zoomScale = Mathf.Clamp(zoomScale + amount, MIN_ZOOM, MAX_ZOOM);
+    }
+    void ZoomIn() => ChangeZoom(-1f);
+    void ZoomOut() => ChangeZoom(1f);
+
+    public void OnZoom(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        float ctxValue = ctx.ReadValue<float>();
+        if(ctxValue < 0)
+        {
+            ZoomOut();
+        }
+        else if(ctxValue > 0)
+        {
+            ZoomIn();
+        }
     }
 }
