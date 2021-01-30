be2fde6ee8ddc853ab7fe430450660f462f810eb
Andrew Nattress <a.nattress@gmail.com>
2021-01-30 14:21:16 +0000

    added controls to ship

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -5,66 +5,26 @@ using UnityEngine.AI;
 //[RequireComponent(typeof(Rigidbody))]
 public class ShipController : MonoBehaviour
 {
-    //public IntVariable playerShips;
-    //public Transform target = null;
-    //public Rigidbody rb;
-
-    //public float rotationSpeed;
-    //public float accelerationSpeed;
-    //public float maxSpeed;
-    //private float speed = 0.0f;
-
-    //void Start()
-    //{
-
-    //    if (rb == null)
-    //        rb = GetComponent<Rigidbody>();
-    //}
-
-    //void Update()
-    //{
-    //    if (target != null)
-    //    {
-    //        MoveTowards();
-    //    }
-    //    else
-    //    {
-    //        speed = rb.velocity.magnitude;
-    //    }
-    //}
-
-    //private void MoveTowards()
-    //{
-    //    if (Vector3.Distance(transform.position, target.position) < 2.0f)
-    //    {
-    //        speed = Mathf.Clamp(Vector3.Distance(transform.position, target.position) - 1.0f, 0.0f, 100.0f);
-    //    }
-    //    else
-    //    {
-    //        speed = Mathf.Lerp(speed, maxSpeed, Time.deltaTime * accelerationSpeed);
-    //    }
-
-
-    //    var lookPos = target.position - transform.position;
-    //    lookPos.y = 0;
-    //    var rotation = Quaternion.LookRotation(lookPos);
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed * Mathf.Clamp(speed, 0.0f, 1.0f));
-
-    //    rb.velocity = transform.forward * speed;
-    //}
-
-    public IntVariable playerShips;
+    public float accelerationSpeed;
+    public float maxSpeed;
 
     public Transform target;
     public NavMeshAgent agent;
 
-
+    Vector3 lastDir;
     private void Update()
     {
-        if(target)
+        if (target)
+        {
+            agent.speed = Mathf.Lerp(agent.speed, maxSpeed, Time.deltaTime * accelerationSpeed);
             agent.destination = target.position;
+            lastDir = transform.position - target.position;
+        }
         else
+        {
+            agent.speed = Mathf.Lerp(agent.speed, 0, Time.deltaTime * accelerationSpeed);
             agent.destination = transform.position;
+        }
     }
     private void OnDrawGizmos()
     {
