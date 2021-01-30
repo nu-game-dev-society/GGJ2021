727af81fea1aabd2ebbb8020f59133aeb1269def
Kitkatann <44240290+Kitkatann@users.noreply.github.com>
2021-01-30 13:06:44 +0000

    ship class

---
+++ Assets/Scripts/ShipCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCombat : MonoBehaviour
+{
+    private ArrayList playerFleet;
+    private ArrayList enemyFleets;
+
+    public ShipCombat()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
