a083e206f776837b9a95dedaf8b31e7f0b936e7e
Patton97 <atpatton97@gmail.com>
2021-01-30 01:01:36 +0000

    Reorganised code
    Ship factory can now create a specified number of ships, and add/remove on demand. Still need to hook this up to key bidnings to test in realtime.

---
+++ Assets/Scripts/ShipFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipFactory : MonoBehaviour
+{
+    public IntVariable playerShips;
+
+    [SerializeField] GameObject shipObject;
+
+    List<GameObject> ships = new List<GameObject>();
+
+    Rigidbody Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Rigidbody = this.GetComponent<Rigidbody>();
+        SpawnShips(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.KeypadPlus))
+            AddShip();
+        if (Input.GetKey(KeyCode.KeypadMinus))
+            RemoveShip();
+
+        this.Rigidbody.velocity = new Vector3(0, 0, 10f);
+    }
+
+    void AddShip()
+    {
+        GameObject newShip = CreateShip(TriangleNumbers.GetRowAndPos(ships.Count + 1));
+        ships.Add(newShip);
+        UpdatePlayerShipCount();
+    }
+
+    void RemoveShip()
+    {
+        GameObject shipToDestroy = ships[ships.Count - 1];
+        ships.Remove(shipToDestroy);
+        Destroy(shipToDestroy);
+        UpdatePlayerShipCount();
+    }
+
+    void UpdatePlayerShipCount()
+    {
+        playerShips.RuntimeValue = ships.Count;
+    }
+
+    GameObject CreateShip(Vector3 position)
+    {
+        GameObject newShip = Instantiate(shipObject, transform.position, Quaternion.identity, transform);
+        newShip.transform.localPosition = position;
+        return newShip;
+    }
+    GameObject CreateShip(int row, int posInRow) => CreateShip(ShipTargetPositionLocator.GetShipTargetPosition(row, posInRow));
+    GameObject CreateShip(Tuple<int, int> rowAndPos) => CreateShip(rowAndPos.Item1, rowAndPos.Item2);
+
+
+    void SpawnShips(int count)
+    {
+        for (int i = 1; i <= count; ++i)
+        {
+            AddShip();
+        }
+    }
+    void SpawnRowsOfShips(int rows) => SpawnShips(TriangleNumbers.GetTriangleNumber(rows));
+}
---
+++ Assets/Scripts/ShipTargetPositionLocator.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ShipTargetPositionLocator
+{ 
+    static float GAPSIZE = 10f;
+
+    /// <summary>
+    /// Gets the target position of the ship at the provided <paramref name="index"/>
+    /// </summary>
+    /// <param name="index">The index of the ship in the fleet</param>
+    /// <returns>The target position of the ship</returns>
+    public static Vector3 GetShipTargetPosition(int index)
+    {
+        System.Tuple<int, int> rowAndPos = TriangleNumbers.GetRowAndPos(index);
+        int row = rowAndPos.Item1;
+        int posInRow = rowAndPos.Item2;
+
+        Debug.Log($"Ship #{index} is in row {row} at position {posInRow}");
+
+        return GetShipTargetPosition(row, posInRow);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="posInRow"></param>
+    /// <returns></returns>
+    public static Vector3 GetShipTargetPosition(int row, int posInRow)
+    {
+        return new Vector3
+        {
+            x = (posInRow - row * 0.5f) * GAPSIZE,
+            y = 0,
+            z = row * GAPSIZE * Mathf.Sin(Mathf.PI / 3f) * -1f
+        };
+    }
+}
