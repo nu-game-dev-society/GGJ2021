a6c647927f4dc44854ee4fca519160db532a7760
josephwhi98 <joseph.whitehead101@gmail.com>
2021-01-30 12:53:40 +0000

    Crew mates walk around deck of ship

---
+++ Assets/Scripts/CrewMate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewMate : MonoBehaviour
+{
+    public Vector3 startPoint;
+    public Transform movementTarget;
+    Vector3 startScale;
+
+    public void Start()
+    {
+        startPoint = transform.localPosition;
+        startScale = transform.localScale; 
+    }
+
+    public void Update()
+    {
+        transform.localPosition = startPoint + ( (movementTarget.localPosition - startPoint) * Mathf.PingPong((Time.time * 0.5f), 1f));
+
+
+        transform.localScale = startScale * (0.8f + Mathf.PingPong((Time.time * 0.5f), 0.2f));
+    }
+}
