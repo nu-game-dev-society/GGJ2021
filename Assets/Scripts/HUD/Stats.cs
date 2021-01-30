9533b00882f3fc866bfa7d56816078d5b7a6e259
rtm516 <ryantmilner@hotmail.co.uk>
2021-01-30 13:17:49 +0000

    Add stats HUD and PirataOne font

---
+++ Assets/Scripts/HUD/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField]
+    private IntVariable playerShips;
+
+    [SerializeField]
+    private IntVariable playerPlunder;
+
+    private TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        string value = "";
+        value += "Yer Armarda: " + playerShips.RuntimeValue.ToString("D2") + "\n";
+        value += "Plunder: " + playerPlunder.RuntimeValue.ToString("D2");
+
+        text.text = value;
+    }
+}
